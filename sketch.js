795c34540fd8ad32295624957e17b2f9f893680d
Hari-Koteru <sudheerreddy.koteru@gmail.com>
2020-12-19 19:02:18 +0530

    syncrhornus ball movement

--- sketch.js
+++ sketch.js
@@ -1,29 +1,61 @@
 var ball;
-
+var database;
+var ballPosition;
+var position;
 function setup(){
+    database = firebase.database(); 
+
     createCanvas(500,500);
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    ballPosition = database.ref("ball/position");
+    //Everytime a change in the database values of position (reference) happens, the readPosition function is called.
+    //If there is any error in reading the values in the database, the showError function is called.
+
+    ballPosition.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if(position !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }  
+        drawSprites();
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+    
+  
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+
+//reading the value of the position in the database
+function readPosition(data){
+    position = data.val();
+    //assign the x and y values of the ball position in the database to the ball sprite.
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("Error in writing to the database")
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+
+
 }
